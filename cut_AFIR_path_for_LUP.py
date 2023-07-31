4232b6a511a01696af36990040cbd14296b1d977
Hiroaki-violine <kryou649@gmail.com>
2023-07-31 19:43:34 +0900

    minor updates

--- cut_AFIR_path_for_LUP.py
+++ cut_AFIR_path_for_LUP.py
@@ -1,7 +1,10 @@
 import pdb
+import os
+import sys
 
 print('\nEnter log name (like xxx.log)')
 log_name = input().split('.log')[0]
+log_path = log_name + '.log'
 
 print('\nEnter initial ITR number for extract')
 init_itr = int(input())
@@ -9,25 +12,29 @@ init_itr = int(input())
 print('\nEnter last ITR number for extract')
 last_itr = int(input())
 
-log_file = open(log_name + '.log', 'r')
+if not os.path.exists(log_path):
+    print(log_path, 'doesn\'t exists...\n')
+    sys.exit(0)
+
+log_file = open(log_path, 'r')
 log_content = log_file.readlines()
 log_file.close()
 
 itr_atom_xyz_l = []
 
-isRead = False
+isWrite = False
 for line in log_content:
     if 'ITR' in line:
         cur_itr = int(line.split()[-1])
         if init_itr <= cur_itr <= last_itr:
-            isRead = True
+            isWrite = True
             itr_atom_xyz = []
 
-    if isRead and 'Item' in line:
-        isRead = False
+    if isWrite and 'Item' in line:
+        isWrite = False
         itr_atom_xyz_l.append('\n')
 
-    if isRead:
+    if isWrite:
         itr_atom_xyz_l.append(line)
 
 cut_log_file = open(log_name + '_cut.log', 'w')
--- gen_xyz_file_from_AFIR_path.py
+++ gen_xyz_file_from_AFIR_path.py
@@ -1,9 +1,16 @@
 import pdb
+import os
+import sys
 
 print('\nEnter log name (like xxx.log)')
 log_name = input().split('.log')[0]
+log_path = log_name + '.log'
 
-log_file = open(log_name + '.log', 'r')
+if not os.path.exists(log_path):
+    print(log_path, 'doesn\'t exists...\n')
+    sys.exit(0)
+
+log_file = open(log_path, 'r')
 log_file_content = log_file.readlines()
 log_file.close()
 
@@ -13,7 +20,7 @@ isWrite = False
 for line in log_file_content:
     if 'ITR' in line:
         isWrite = True
-    if 'Item' in line:
+    if isWrite and 'Item' in line:
         isWrite = False
         xyz_file.write('\n')
     if isWrite:
