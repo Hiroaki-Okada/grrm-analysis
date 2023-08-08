09565d053ad1566f6fadc9bbdabf3c2f0465b3d1
Hiroaki-violine <kryou649@gmail.com>
2023-08-08 17:18:13 +0900

    Improved standard output

--- gen_xyz_file_from_IRC_path.py
+++ gen_xyz_file_from_IRC_path.py
@@ -1,58 +1,91 @@
 import pdb
+
 import os
 import sys
 
-print('\nEnter log name (like xxx.log)')
-log_name = input().split('.log')[0]
-log_path = log_name + '.log'
 
-if not os.path.exists(log_path):
-    print(log_path, 'doesn\'t exists...\n')
-    sys.exit(0)
+def define():
+    print('\nEnter log name (like xxx.log)')
+    log_name = input().split('.log')[0]
+    log_path = log_name + '.log'
 
-log_file = open(log_path, 'r')
-log_file_content = log_file.readlines()
-log_file.close()
+    if not os.path.exists(log_path):
+        print('\n{} doesn\'t exists...\n'.format(log_path))
+        sys.exit(0)
 
-isTSRead = False
-isIRCRead = False
-ts_l = []
-irc_l = []
+    log_file = open(log_path, 'r')
+    log_file_content = log_file.readlines()
+    log_file.close()
 
-for idx, line in enumerate(log_file_content):
-    if 'INITIAL STRUCTURE' in line:
-        isTSRead = True
-        ts = []
+    return log_name, log_file_content
 
-    if isTSRead and 'ENERGY' in line:
-        isTSRead = False
-        ts.append('\n')
-        ts_l.append(ts)
 
-    if ts_l and any((i in line for i in ('# STEP', '# ITR'))):
-        isIRCRead = True
-        irc = []
+def gen_irc_xyz(log_name, log_file_content):
+    isTSRead = False
+    isIRCRead = False
+    ts_l = []
+    irc_l = []
 
-    if isIRCRead and any((i in line for i in ('ENERGY', 'Item'))):
-        isIRCRead = False
-        irc.append('\n')
-        irc_l.append(irc)
+    for idx, line in enumerate(log_file_content):
+        # Start TS extraction
+        if 'INITIAL STRUCTURE' in line:
+            isTSRead = True
+            ts = []
 
-    if isTSRead:
-        ts.append(line)
+        # Finish TS extraction
+        if isTSRead and 'ENERGY' in line:
+            isTSRead = False
+            ts.append('\n')
+            ts_l.append(ts)
 
-    if isIRCRead:
-        irc.append(line)
+        # Start IRC extraction
+        if ts_l and any((i in line for i in ('# STEP', '# ITR'))):
+            isIRCRead = True
+            irc = []
 
-    if line == '# STEP 1\n':
-        middle_idx = len(irc_l)
+        # Finish IRC extraction
+        if isIRCRead and any((i in line for i in ('ENERGY', 'Item'))):
+            isIRCRead = False
+            irc.append('\n')
+            irc_l.append(irc)
 
-irc_path = irc_l[:middle_idx][::-1] + ts_l + irc_l[middle_idx:]
+        # Extract TS
+        if isTSRead:
+            ts.append(line)
 
-xyz_file = open(log_name + '.xyz', 'w')
+        # Extraxt IRC
+        if isIRCRead:
+            irc.append(line)
 
-for i in irc_path:
-    for j in i:
-        xyz_file.write(j)
+        # Index of turning point for IRC path
+        if line == '# STEP 1\n':
+            middle_idx = len(irc_l)
 
-xyz_file.close()
+    irc_path = irc_l[:middle_idx][::-1] + ts_l + irc_l[middle_idx:]
+
+    xyz_file = open(log_name + '.xyz', 'w')
+
+    for i_idx, i in enumerate(irc_path):
+        for j_idx, j in enumerate(i):
+            if j_idx == 0:
+                if i_idx < middle_idx:
+                    xyz_file.write('{} ({})\n'.format(j.rstrip('\n'), 'forward'))
+                elif i_idx > middle_idx:
+                    xyz_file.write('{} ({})\n'.format(j.rstrip('\n'), 'backward'))
+                else:
+                    xyz_file.write('TS structure\n')
+            else:
+                xyz_file.write(j)
+
+    xyz_file.close()
+
+    print('\n{}.xyz was generated.\n'.format(log_name))
+
+
+def run():
+    log_name, log_file_content = define()
+    gen_irc_xyz(log_name, log_file_content)
+
+
+if __name__ == '__main__':
+    run()
