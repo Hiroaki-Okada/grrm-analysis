9cab8c0c6c98edcecbf4ee6f18ec3d82fc92ac28
Hiroaki-violine <kryou649@gmail.com>
2023-08-14 19:03:30 +0900

    add FrozenAtoms reader

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ Source code to analyze GRRM log files.
 ### cut_AFIR_path_for_LUP.py
 Cut out specified regions of the AFIR path to reduce the cost of LUP calculations.
 
-### gen_xyz_from_AFIR_path.py
-Generate a xyz file (for Chemcraft) from a AFIR path of GRRM log file.
+### gen_xyz_from_opt_log.py
+Generate a xyz file (for Chemcraft) from a ITR of GRRM log file. FrozenAtoms are also written.
 
 ### gen_xyz_from_IRC_path.py
 Generate a xyz file (for Chemcraft) from a IRC path of GRRM log file.
--- gen_xyz_file_from_opt_log.py
+++ gen_xyz_file_from_opt_log.py
@@ -8,24 +8,49 @@ def get_log_name():
     print('\nEnter log name (like xxx.log)')
     log_name = input().split('.log')[0]
     log_path = log_name + '.log'
+    com_path = log_name + '.com'
 
     if not os.path.exists(log_path):
         print('\n{} doesn\'t exists...\n'.format(log_path))
+        sys.exit(0)
+    if not os.path.exists(com_path):
+        print('\n{} doesn\'t exists...\n'.format(com_path))
         sys.exit(0)
 
     return log_name
 
 
-def generate_xyz_file(log_name, log_file_content):
+def read_com_file(com_file_content):
+    isRead = False
+    frozen_atoms = []
+    for line in com_file_content:
+        if 'options' in line.lower():
+            isRead = False
+
+        if isRead:
+            frozen_atoms.append(line)
+
+        if 'frozenatoms' in line.lower():
+            isRead = True
+
+    return frozen_atoms
+
+
+def generate_xyz_file(log_name, log_file_content, frozen_atoms):
     xyz_file = open(log_name + '.xyz', 'w')
 
     isWrite = False
     for line in log_file_content:
         if 'ITR' in line:
             isWrite = True
+
         if isWrite and 'Item' in line:
             isWrite = False
+            if frozen_atoms != []:
+                for i in frozen_atoms:
+                    xyz_file.write(i)
             xyz_file.write('\n')
+
         if isWrite:
             xyz_file.write(line)
 
@@ -37,7 +62,12 @@ def run():
     with open(log_name + '.log', 'r') as log_file:
         log_file_content = log_file.readlines()
 
-    generate_xyz_file(log_name, log_file_content)
+    with open(log_name + '.com', 'r') as com_file:
+        com_file_content = com_file.readlines()
+
+    frozen_atoms = read_com_file(com_file_content)
+    generate_xyz_file(log_name, log_file_content, frozen_atoms)
+
     print('\n{}.xyz was generated.\n'.format(log_name))
 
 
